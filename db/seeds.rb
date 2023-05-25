dfb2c197b590384da171ea8407cf5431e096355e
William Chan <chan.william.ts@gmail.com>
2023-05-25 15:19:19 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning db"
+Movie.destroy_all
+
+10.times do
+  movie = Movie.create!(
+    title: Faker::Movie.title,
+    overview: Faker::GreekPhilosophers.name,
+    poster_url: Faker::Creature::Animal,
+    rating: rand(0..10)
+  )
+  puts "Movie with id #{movie.id} was created"
+end
+
+puts 'All done'
